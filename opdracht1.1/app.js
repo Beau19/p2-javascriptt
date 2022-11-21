230ab73a3161d30d798cfb963d2e3142756a84f8
Beau19 <9019316@student.zadkine.nl>
2022-11-21 20:48:15 +0100

    javascript 1.1

---
+++ opdracht1.1/app.js
@@ -0,0 +1,9 @@
+let paragraaf = document.getElementById("winkelmand")
+let shop = ["Bananen", "Whisky", "Chips", "Bonen", "Aardappelen"];
+
+paragraaf.innerText ="er zitten "+  shop.length + " items in onze array";
+
+paragraaf.innerText = shop
+
+// paragraaf.innerText = Bonen;
+
