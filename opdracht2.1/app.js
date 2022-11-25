34d48beda4485e9b07855b021a76a6c40405f74f
Beau19 <9019316@student.zadkine.nl>
2022-11-25 09:49:59 +0100

    javascript 2.1

--- opdracht2.1/app.js
+++ opdracht2.1/app.js
@@ -1,12 +1,11 @@
 let block = document.getElementById("div");
 let position = 0;
-
 function move() {
     if (position >= window.innerWidth) {
         position = 0;
     } else {
-        position += 50;
-        block.style.left = position = "px";
+      position += 50;
+      block.style.left = position + "px";
     }
 }
 
